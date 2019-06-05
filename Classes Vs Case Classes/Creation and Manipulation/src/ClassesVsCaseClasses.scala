51e9dac71ea4f012a21c0981ddc37d84a89d3d9e
Geravant <igor.gera.gerasim@gmail.com>
2019-06-05 13:52:05 +0300

    Classes vs Case Classes - Creation and Manipulation: bankAccount manipulation added

--- Classes Vs Case Classes/Creation and Manipulation/src/ClassesVsCaseClasses.scala
+++ Classes Vs Case Classes/Creation and Manipulation/src/ClassesVsCaseClasses.scala
@@ -3,8 +3,10 @@ import org.scalatest.{FlatSpec, Matchers}
 object ClassesVsCaseClasses extends FlatSpec with Matchers {
   def creationAndManipulation(): Unit = {
     val aliceAccount = new BankAccount
+    aliceAccount.deposit(100)
     val c3           = Note("C", "Quarter", 3)
 
     c3.name shouldBe "C"
+    aliceAccount.withdraw(50) shouldBe 50
   }
 }
