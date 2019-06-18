25d4644544f50fbc20d542ace362423dcfa68b4a
Geravant <igor.gera.gerasim@gmail.com>
2019-06-18 15:33:10 +0300

    Classes Vs Case Classes - Creation and Manipulation: task file large blank fixed

--- Classes Vs Case Classes/Creation and Manipulation/src/ClassesVsCaseClasses.scala
+++ Classes Vs Case Classes/Creation and Manipulation/src/ClassesVsCaseClasses.scala
@@ -4,7 +4,7 @@ object ClassesVsCaseClasses extends FlatSpec with Matchers {
   def creationAndManipulation(): Unit = {
     val aliceAccount = new BankAccount
     aliceAccount.deposit(100)
-    val c3           = Note("C", "Quarter", 3)
+    val c3 = Note("C", "Quarter", 3)
 
     c3.name shouldBe "C"
     aliceAccount.withdraw(50) shouldBe 50
