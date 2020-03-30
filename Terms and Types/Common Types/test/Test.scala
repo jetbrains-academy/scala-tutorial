9ff221bd7ccc870e34ed9599b2439739e62d8ba7
Geravant <igor.gera.gerasim@gmail.com>
2020-03-30 11:37:40 +0300

    fix(Terms and Types - Common Types ): Tests reworked
    Matchers moved from the task file to the test file, standard method
    calls split into different methods, main method added, placeholders
    rearranged accordingly
    
    Closes EDC-214

--- Terms and Types/Common Types/test/Test.scala
+++ Terms and Types/Common Types/test/Test.scala
@@ -1,8 +1,21 @@
-import org.scalatest.Spec
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers {
 
-  def `check more methods`(): Unit = {
-    TermsAndTypes.moreMethods()
+  def `check toHexString method`(): Unit = {
+    TermsAndTypes.toHexStringMethod() shouldBe "10"
+  }
+
+  def `check contains method`(): Unit = {
+    TermsAndTypes.containsMethod() shouldBe true
+  }
+
+  def `check drop method`() : Unit = {
+    TermsAndTypes.dropMethod() shouldBe "oo"
+  }
+
+  def `check take method`(): Unit = {
+    TermsAndTypes.takeMethod() shouldBe "ba"
   }
 }
