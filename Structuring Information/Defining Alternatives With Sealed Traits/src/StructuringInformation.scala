e9d39678ae433b75f14905df7448dea011aea3b6
Geravant <igor.gera.gerasim@gmail.com>
2020-04-03 17:24:28 +0300

    fix(Structuring Information - Defining Alternatives with Sealed Traits): Tests reworked
    Assertions moved from the task file to the test file, code rearranged,
    main method added, placeholders modified accordingly
    
    Closes EDC-214

--- Structuring Information/Defining Alternatives With Sealed Traits/src/StructuringInformation.scala
+++ Structuring Information/Defining Alternatives With Sealed Traits/src/StructuringInformation.scala
@@ -1,13 +1,30 @@
 import org.scalatest.{FlatSpec, Matchers}
 
 object StructuringInformation extends FlatSpec with Matchers {
+
   sealed trait Symbol
-  def caseClassEquals(): Unit = {
-    case class Note(name: String, duration: String, octave: Int) extends Symbol
+  case class Note(name: String, duration: String, octave: Int) extends Symbol
+  case class Rest(duration: String) extends Symbol
+
+  def symbolDuration(symbol: Symbol): String =
+    symbol match {
+    case Note(name, duration, octave) => duration
+    case Rest(duration) => duration
+  }
+
+  def caseClassEquals(note: Note, otherNote: Note): Boolean = {
+    note == otherNote
+  }
+
+  def main(args: Array[String]): Unit = {
     val c3      = Note("C", "Quarter", 3)
     val otherC3 = Note("C", "Quarter", 3)
     val f3      = Note("F", "Quarter", 3)
-    (c3 == otherC3) shouldBe true
-    (c3 == f3) shouldBe false
+    val wholeRest = Rest("Whole")
+
+    println("c3 equals to otherC3: " + caseClassEquals(c3, otherC3))
+    println("The duration of the c3 is: " + symbolDuration(c3))
+    println("c3 equals to f3: " + caseClassEquals(c3, f3))
+    println("The duration of the wholeRest is: " + symbolDuration(wholeRest))
   }
 }
--- Structuring Information/Defining Alternatives With Sealed Traits/test/Test.scala
+++ Structuring Information/Defining Alternatives With Sealed Traits/test/Test.scala
@@ -1,9 +1,32 @@
-import org.scalatest.Spec
+import StructuringInformation.{Note, Rest}
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers{
 
   def `check class equals`(): Unit = {
-    StructuringInformation.caseClassEquals()
+    //Arrange
+    val c1     = Note("C", "Quarter", 1)
+    val otherC1 = Note("C", "Quarter", 1)
+
+    //Assert
+    StructuringInformation.caseClassEquals(c1, otherC1) shouldBe true
+
+    //Arrange
+    val c2 = Note("C", "Whole", 2)
+
+    //Assert
+    StructuringInformation.caseClassEquals(c1, c2) shouldBe false
+
+    //Assert
+    StructuringInformation.caseClassEquals(c1, c1) shouldBe true
+  }
+
+  def `check Rest extends Symbol`(): Unit = {
+    def isSymbol[P](implicit m: Manifest[P]): Boolean =
+      classOf[StructuringInformation.Symbol].isAssignableFrom(m.erasure)
+
+    isSymbol[Rest] shouldBe true
   }
 
 }
