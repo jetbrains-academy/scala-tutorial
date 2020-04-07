4ed1cfaed810d96fa4fdf543e33fa8e2c0d8381c
Geravant <igor.gera.gerasim@gmail.com>
2020-04-07 11:57:36 +0300

    fix(Standard Library - List): Tests reworked
    Assertion s moved from the task file to the test file, main method added
    
    Closes EDC-214

--- Standard Library/List/src/StandardLibrary.scala
+++ Standard Library/List/src/StandardLibrary.scala
@@ -1,17 +1,14 @@
-import org.scalatest.{FlatSpec, Matchers}
+object StandardLibrary{
+  val cond: (Int, Int) => Boolean = (_: Int) < (_: Int)
+  def insert(x: Int, xs: List[Int]): List[Int] =
+    xs match {
+      case List() => x :: List.empty[Int]
+      case y :: ys =>
+        if (cond(x, y)) x :: y :: ys
+        else y :: insert(x, ys)
+    }
 
-object StandardLibrary extends FlatSpec with Matchers {
-  def insertionSort(): Unit = {
-    val cond: (Int, Int) => Boolean = (_: Int) < (_: Int)
-    def insert(x: Int, xs: List[Int]): List[Int] =
-      xs match {
-        case List() => x :: List.empty[Int]
-        case y :: ys =>
-          if (cond(x, y)) x :: y :: ys
-          else y :: insert(x, ys)
-      }
-    insert(2, 1 :: 3 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
-    insert(1, 2 :: 3 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
-    insert(3, 1 :: 2 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
+  def main(args: Array[String]): Unit = {
+    println(insert(4, 1::8::Nil))
   }
 }
--- Standard Library/List/test/Test.scala
+++ Standard Library/List/test/Test.scala
@@ -1,9 +1,14 @@
-import org.scalatest.Spec
+import StandardLibrary.insert
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers {
 
   def `check insertion sort`(): Unit = {
-    StandardLibrary.insertionSort()
+
+    insert(2, 1 :: 3 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
+    insert(1, 2 :: 3 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
+    insert(3, 1 :: 2 :: Nil) shouldBe (1 :: 2 :: 3 :: Nil)
   }
 
 }
