ff3132be95a17d3bca676a1794ed2be19a5cff34
Geravant <igor.gera.gerasim@gmail.com>
2020-04-13 13:14:17 +0300

    fix(Syntactic conviniences - Method Parameters): Tests rework
    Assertions moved to the test file, main method added, placeholders
    rearranged
    
    Closes EDC-214

--- Syntactic Conveniences/Methods Parameters/src/SyntacticConveniences.scala
+++ Syntactic Conveniences/Methods Parameters/src/SyntacticConveniences.scala
@@ -1,31 +1,29 @@
-import org.scalatest.{FlatSpec, Matchers}
+object SyntacticConveniences {
 
-object SyntacticConveniences extends FlatSpec with Matchers {
+  case class Range(start: Int, end: Int, step: Int = 1)
 
-  def defaultParameters(): Unit = {
-    case class Range(start: Int, end: Int, step: Int = 1)
 
+  def average(x: Int, xs: Int*): Double =
+    (x :: xs.to[List]).sum.toDouble / (xs.size + 1)
+
+
+  type Result = Either[String, (Int, Int)]
+  def divide(dividend: Int, divisor: Int): Result =
+    if (divisor == 0) Left("Division by zero")
+    else Right((dividend / divisor, dividend % divisor))
+
+  def main(args: Array[String]): Unit = {
     val xs = Range(start = 1, end = 10)
+    println("The step:")
+    println(xs.step)
 
-    xs.step shouldBe 1
-  }
+    println("average result:")
+    println(average(1))
+    println(average(1, 3))
+    println(average(1, 3, 5, 7))
 
-  def repeatedParameters(): Unit = {
-    def average(x: Int, xs: Int*): Double =
-      (x :: xs.to[List]).sum.toDouble / (xs.size + 1)
-
-    average(1) shouldBe 1.0
-    average(1, 2) shouldBe 1.5
-    average(1, 2, 3) shouldBe 2.0
-  }
-
-  def typeAlias(): Unit = {
-    type Result = Either[String, (Int, Int)]
-    def divide(dividend: Int, divisor: Int): Result =
-      if (divisor == 0) Left("Division by zero")
-      else Right((dividend / divisor, dividend % divisor))
-    divide(6, 4) shouldBe Right((1, 2))
-    divide(2, 0) shouldBe Left("Division by zero")
-    divide(8, 4) shouldBe (Right((2, 0)): Either[String, (Int, Int)])
+    println("divide result:")
+    println(divide(4, 2))
+    println(divide(5, 0))
   }
 }
--- Syntactic Conveniences/Methods Parameters/test/Test.scala
+++ Syntactic Conveniences/Methods Parameters/test/Test.scala
@@ -1,17 +1,26 @@
-import org.scalatest.Spec
+import SyntacticConveniences.{Range, average, divide}
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers{
 
   def `default parameters`(): Unit = {
-    SyntacticConveniences.defaultParameters()
+    val xs = Range(start = 1, end = 10)
+
+    xs.step shouldBe 1
   }
 
   def `repeated parameters`(): Unit = {
-    SyntacticConveniences.repeatedParameters()
+    average(1) shouldBe 1.0
+    average(1, 2) shouldBe 1.5
+    average(1, 2, 3) shouldBe 2.0
   }
 
   def `type alias`(): Unit = {
-    SyntacticConveniences.typeAlias()
+
+    divide(6, 4) shouldBe Right((1, 2))
+    divide(2, 0) shouldBe Left("Division by zero")
+    divide(8, 4) shouldBe (Right((2, 0)): Either[String, (Int, Int)])
   }
 
 }
