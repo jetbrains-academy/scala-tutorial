a939e37183fa8b22469a120ba351383bb977ce53
Geravant <igor.gera.gerasim@gmail.com>
2020-04-14 11:42:18 +0300

    fix(Imperative Programming - Imperative Loops): Tests reworked
    Assertion moved to the test file, placeholders uipdated, main method
    added
    
    Closes EDC-214

--- Imperative Programming/Imperative Loops/src/ImperativeProgramming.scala
+++ Imperative Programming/Imperative Loops/src/ImperativeProgramming.scala
@@ -1,21 +1,17 @@
-import org.scalatest.{FlatSpec, Matchers}
-
-object ImperativeProgramming extends FlatSpec with Matchers {
-
-    def factorialExercise(): Unit = {
-      def factorial(n: Int): Int = {
-        var result = 1
-        var i      = 2
-        while (i <= n) {
-          result = result * i
-          i = i + 1
-        }
-        result
-      }
-
-      factorial(2) shouldBe 2
-      factorial(3) shouldBe 6
-      factorial(4) shouldBe 24
-      factorial(5) shouldBe 120
+object ImperativeProgramming{
+  def factorial(n: Int): Int = {
+    var result = 1
+    var i      = 2
+    while (i <= n) {
+      result = result * i
+      i = i + 1
     }
+    result
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(factorial(5))
+    println(factorial(11))
+    println(factorial(15))
+  }
 }
--- Imperative Programming/Imperative Loops/test/Test.scala
+++ Imperative Programming/Imperative Loops/test/Test.scala
@@ -1,8 +1,14 @@
-import org.scalatest.Spec
+import ImperativeProgramming.factorial
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers {
 
   def `test factorial`(): Unit = {
-    ImperativeProgramming.factorialExercise()
+
+    factorial(2) shouldBe 2
+    factorial(3) shouldBe 6
+    factorial(4) shouldBe 24
+    factorial(5) shouldBe 120
   }
 }
