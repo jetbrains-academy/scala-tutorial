6af2c5d96493e6b4ef5379140d88622959b33230
Geravant <igor.gera.gerasim@gmail.com>
2020-04-15 15:03:16 +0300

    fix(Type Classes - Type Classes): Tests Rework
    Assertion moved to the test file, placeholder hind modified, main
    method added
    
    Closes EDC-214

--- Type Classes/Type Classes/src/TypeClasses.scala
+++ Type Classes/Type Classes/src/TypeClasses.scala
@@ -1,28 +1,24 @@
-import org.scalatest.{FlatSpec, Matchers}
+object TypeClasses {
+  /**
+    * Returns an integer whose sign communicates how the first parameter
+    * compares to the second parameter.
+    *
+    * The result sign has the following meaning:
+    *  - Negative if the first parameter is less than the second parameter
+    *  - Positive if the first parameter is greater than the second parameter
+    *  - Zero otherwise
+    */
+  val compareRationals: (Rational, Rational) => Int = (x: Rational, y: Rational) => x.numer * y.denom - y.numer * x.denom
 
-object TypeClasses extends FlatSpec with Matchers {
-  def rationalOrdering(): Unit = {
+  implicit val rationalOrder: Ordering[Rational] =
+    (x: Rational, y: Rational) => compareRationals(x, y)
 
-    /**
-      * Returns an integer whose sign communicates how the first parameter
-      * compares to the second parameter.
-      *
-      * The result sign has the following meaning:
-      *  - Negative if the first parameter is less than the second parameter
-      *  - Positive if the first parameter is greater than the second parameter
-      *  - Zero otherwise
-      */
-    val compareRationals: (Rational, Rational) => Int = (x: Rational, y: Rational) => x.numer * y.denom - y.numer * x.denom
-
-    implicit val rationalOrder: Ordering[Rational] =
-      new Ordering[Rational] {
-        def compare(x: Rational, y: Rational): Int = compareRationals(x, y)
-      }
-
+  def main(args: Array[String]): Unit = {
     val half      = new Rational(1, 2)
     val third     = new Rational(1, 3)
     val fourth    = new Rational(1, 4)
     val rationals = List(third, half, fourth)
-    Sorting.insertionSort(rationals) shouldBe List(fourth, third, half)
+    println(Sorting.insertionSort(rationals) == List(fourth, third, half))
   }
+
 }
--- Type Classes/Type Classes/test/Test.scala
+++ Type Classes/Type Classes/test/Test.scala
@@ -1,8 +1,14 @@
-import org.scalatest.Spec
+import TypeClasses.rationalOrder
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers{
 
   def `rational Ordering`(): Unit = {
-    TypeClasses.rationalOrdering()
+    val testThird      = new Rational(1, 3)
+    val testHalf     = new Rational(2, 4)
+    val testSixth    = new Rational(1, 6)
+    val rationals = List(testThird, testHalf, testSixth)
+    Sorting.insertionSort(rationals) shouldBe List(testSixth, testThird, testHalf)
   }
 }
