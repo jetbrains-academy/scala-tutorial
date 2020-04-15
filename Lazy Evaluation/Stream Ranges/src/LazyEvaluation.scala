981087119dce64e52708beeb743dc4e0a619a3cb
Geravant <igor.gera.gerasim@gmail.com>
2020-04-15 13:39:24 +0300

    fix(LAzy Evaluation - Stream Ranges): Tests reworked
    Assertions moved to the test file, main method added, placeholder
    rearranged

--- Lazy Evaluation/Stream Ranges/src/LazyEvaluation.scala
+++ Lazy Evaluation/Stream Ranges/src/LazyEvaluation.scala
@@ -1,14 +1,21 @@
 import org.scalatest.{FlatSpec, Matchers}
 
-object LazyEvaluation extends FlatSpec with Matchers {
-  def streamRangeExercise(): Unit = {
-    var rec = 0
-    def streamRange(lo: Int, hi: Int): Stream[Int] = {
-      rec = rec + 1
-      if (lo >= hi) Stream.empty
-      else Stream.cons(lo, streamRange(lo + 1, hi))
-    }
+object LazyEvaluation {
+  var rec = 0
+  def streamRange(lo: Int, hi: Int): Stream[Int] = {
+    rec = rec + 1
+    if (lo >= hi) Stream.empty
+    else Stream.cons(lo, streamRange(lo + 1, hi))
+  }
+
+  def main(args: Array[String]): Unit = {
     streamRange(1, 10).take(3).toList
-    rec shouldBe 3
+    println(rec)
+
+    streamRange(1, 10).take(5).toList
+    println(rec)
+
+    streamRange(1, 10).take(3).toList
+    println(rec)
   }
 }
--- Lazy Evaluation/Stream Ranges/test/Test.scala
+++ Lazy Evaluation/Stream Ranges/test/Test.scala
@@ -1,8 +1,18 @@
-import org.scalatest.Spec
+import LazyEvaluation.{rec, streamRange}
+import org.scalatest.Matchers
+import org.scalatest.refspec.RefSpec
 
-class Test extends Spec {
+class Test extends RefSpec with Matchers{
 
   def `test encoding`(): Unit = {
-    LazyEvaluation.streamRangeExercise()
+    streamRange(1, 10).take(3).toList
+    rec shouldBe 3
+
+    streamRange(1, 10).take(1).toList
+    rec shouldBe 4
+
+
+    streamRange(1, 10).take(2).toList
+    rec shouldBe 6
   }
 }
