283e43db9b5f3a2d45e804daafe1131bf7507f63
sofiiako <19rubisco93@gmail.com>
2021-07-20 13:12:07 +0300

    Updated task and test code in Stream Ranges

--- Lazy Evaluation/Stream Ranges/src/LazyEvaluation.scala
+++ Lazy Evaluation/Stream Ranges/src/LazyEvaluation.scala
@@ -1,21 +1,19 @@
-import org.scalatest.{FlatSpec, Matchers}
+import scala.collection.compat.immutable.LazyList
 
 object LazyEvaluation {
   var rec = 0
-  def streamRange(lo: Int, hi: Int): Stream[Int] = {
+  def llRange(lo: Int, hi: Int): LazyList[Int] = {
     rec = rec + 1
-    if (lo >= hi) Stream.empty
-    else Stream.cons(lo, streamRange(lo + 1, hi))
+    if (lo >= hi) LazyList.empty
+    else LazyList.cons(lo, llRange(lo + 1, hi))
   }
-
   def main(args: Array[String]): Unit = {
-    streamRange(1, 10).take(3).toList
+    llRange(1, 10).take(3).toList
+    println(rec)
+    llRange(1, 10).take(5).toList
+    println(rec)
+    llRange(1, 10).take(3).toList
     println(rec)
 
-    streamRange(1, 10).take(5).toList
-    println(rec)
-
-    streamRange(1, 10).take(3).toList
-    println(rec)
   }
 }
--- Lazy Evaluation/Stream Ranges/test/Test.scala
+++ Lazy Evaluation/Stream Ranges/test/Test.scala
@@ -1,18 +1,14 @@
-import LazyEvaluation.{rec, streamRange}
-import org.scalatest.Matchers
+import LazyEvaluation.{llRange, rec}
+import org.scalatest.matchers.should.Matchers
 import org.scalatest.refspec.RefSpec
 
 class Test extends RefSpec with Matchers{
-
   def `test encoding`(): Unit = {
-    streamRange(1, 10).take(3).toList
-    rec shouldBe 3
-
-    streamRange(1, 10).take(1).toList
+    llRange(1, 10).take(3).toList
     rec shouldBe 4
-
-
-    streamRange(1, 10).take(2).toList
+    llRange(1, 10).take(1).toList
     rec shouldBe 6
+    llRange(1, 10).take(2).toList
+    rec shouldBe 9
   }
 }
